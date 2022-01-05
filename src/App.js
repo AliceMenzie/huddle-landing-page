0cbf31b6d224fccf7902f3d27deb2eb59b454407
AliceMenzie <alice.menzie@gmail.com>
2022-01-05 12:34:21 +1100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Container } from "./components/styles/Container.styled";
+import Header from "./components/Header";
+function App() {
+  return (
+    <>
+      <Header />
+      <Container>hello</Container>
+    </>
+  );
+}
+
+export default App;
