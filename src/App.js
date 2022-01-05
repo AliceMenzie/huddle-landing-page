43313ee91037563c04744195602e32c363de7edd
AliceMenzie <alice.menzie@gmail.com>
2022-01-05 13:05:40 +1100

    add initial styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import {ThemeProvider} from 'styled-components'
 import { Container } from "./components/styles/Container.styled";
 import Header from "./components/Header";
+
 function App() {
+
+  const theme = {
+    colors: {
+      header: '#ebfbff',
+      body: 'white',
+      footer: '003333',
+
+    },
+  }
+
   return (
+    <ThemeProvider theme = {theme}>
     <>
       <Header />
       <Container>hello</Container>
     </>
+    </ThemeProvider>
   );
 }
 
