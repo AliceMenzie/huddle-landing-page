addec4aba4d6440f6e1cbf2cc4c2cc1a139988e4
AliceMenzie <alice.menzie@gmail.com>
2022-01-05 15:44:43 +1100

    add alternation to cards body styling

--- src/Card.js
+++ src/Card.js
@@ -1,8 +1,10 @@
 import { StyledCard } from "./components/styles/Card.styled"
 
 export default function Card({item: {id, title, body, image}}) {
+
+
     return (
-        <StyledCard>
+        <StyledCard layout= {id % 2 === 0 && 'row-reverse' }>
             <div>
                 <h2>{title}</h2>
                 <p>{body}</p>
